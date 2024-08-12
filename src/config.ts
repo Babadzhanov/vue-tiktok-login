9ad217ed426d99455e2d49e254d6e18d7a84b332
Babadzhanov <simeoncoding@gmail.com>
2024-08-12 20:53:18 +0100

    manage env vars

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export default {
+  tiktok: {
+    id: import.meta.env.VITE_TIKTOK_CLIENT_ID,
+    key: import.meta.env.VITE_TIKTOK_CLIENT_KEY,
+    secret: import.meta.env.VITE_TIKTOK_CLIENT_SECRET
+  }
+}
