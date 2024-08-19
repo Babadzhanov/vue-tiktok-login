3ed87ca88c99114c84cfdd7ea41b88490343b562
Babadzhanov <simeoncoding@gmail.com>
2024-08-19 13:51:46 +0100

    make the redirect at root, add loading and setAuthStore then redirect to user

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,55 +1,64 @@
 <script setup lang="ts">
 // import { onMounted } from 'vue'
 import axios from 'axios'
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
+const router = useRouter()
+
+const isLoading = ref(false)
 
 // Get the TikTok authorization URL
 const loginWithTikTok = async () => {
+  isLoading.value = true
   const response = await axios.get('https://vue-tiktok-login-server.onrender.com/oauth')
   console.log(`getAuthUrl's response:`, response)
   window.location.href = `${response.data.url}`
 }
 
-// Handle the TikTok redirect
-// function handleTikTokRedirect() {
-//   const urlParams = new URLSearchParams(window.location.search)
-//   const code = urlParams.get('code')
-//   const scopes = urlParams.get('scopes')
-//   const state = urlParams.get('state')
+async function handleTikTokRedirect() {
+  const urlSearchParams = new URLSearchParams(window.location.search)
+  const code = urlSearchParams.get('code')
+  const scopes = urlSearchParams.get('scopes')
+  const state = urlSearchParams.get('state')
+  console.log('Authorization code:', code)
+  console.log('Scopes:', scopes)
+  console.log('State:', state)
 
-//   if (code) {
-//     exchangeCodeForToken(code)
-//     console.log('Authorization code:', code)
-//     console.log('Scopes:', scopes)
-//     console.log('State:', state)
+  if (code) {
+    try {
+      const response = await axios.post(
+        'https://vue-tiktok-login-server.onrender.com/accesstoken',
+        {
+          code
+        }
+      )
+      console.log(`response`, response)
+      const accessToken = response.data.access_token
+      console.log('Access Token:', accessToken)
+      authStore.setAccessToken(accessToken)
+      router.push('/user')
+    } catch (error) {
+      console.error('Error exchanging code for token:', error)
+    }
 
-//     // remove the code from the URL
-//     window.history.replaceState({}, document.title, window.location.pathname)
-//   }
-// }
+    // Clear the code from the URL
+    window.history.replaceState({}, document.title, window.location.pathname)
+  }
+}
 
-// Exchange the authorization code for an access token
-// async function exchangeCodeForToken(code: string) {
-//   try {
-//     const response = await axios.post('https://dev.vervesearch.com/_temp/tiktok-login-v2/token', {
-//       code
-//     })
-//     console.log(`response`, response)
-//     const accessToken = response.data.data.access_token
-//     console.log('Access Token:', accessToken)
-//   } catch (error) {
-//     console.error('Error exchanging code for token:', error)
-//   }
-// }
-
-// onMounted(() => {
-//   handleTikTokRedirect()
-// })
+onMounted(() => {
+  handleTikTokRedirect()
+})
 </script>
 
 <template>
   <div class="login">
     <h1 class="green">{{ 'Login' }}</h1>
-    <button class="button-login" @click="loginWithTikTok">
+    <div v-if="isLoading" class="loading-spinner"></div>
+    <button v-else class="button-login" @click="loginWithTikTok">
       <img alt="Tiktok login button" class="logo" src="@/assets/button.svg" />
     </button>
   </div>
@@ -74,6 +83,24 @@ h1 {
   h1,
   h3 {
     text-align: center;
+  }
+}
+
+.loading-spinner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-top: 4px solid $color-primary;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 
---
+++ src/stores/auth.ts
@@ -0,0 +1,12 @@
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    accessToken: null as string | null
+  }),
+  actions: {
+    setAccessToken(token: string) {
+      this.accessToken = token
+    }
+  }
+})
